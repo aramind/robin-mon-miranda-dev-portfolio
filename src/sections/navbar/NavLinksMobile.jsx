80715eee274eaab74942e0c09a627ed43af92bcf
aramind <88814622+aramind@users.noreply.github.com>
2025-05-25 23:51:49 +0800

    feat: centering the links for mobile

--- src/sections/navbar/NavLinksMobile.jsx
+++ src/sections/navbar/NavLinksMobile.jsx
@@ -1,4 +1,12 @@
-import { IconButton, Link, Menu, MenuItem, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  IconButton,
+  Link,
+  Menu,
+  MenuItem,
+  Typography,
+} from "@mui/material";
 import React, { useState } from "react";
 import useActiveSection from "../../hooks/useActiveSection";
 import { HamburgerIcon } from "../../utils/muiIcons";
@@ -26,22 +34,34 @@ const NavLinksMobile = ({ sections }) => {
         open={Boolean(anchorMenu)}
         anchorEl={anchorMenu}
         onClose={handleCloseMenu}
+        slotProps={{
+          paper: {
+            sx: {
+              backgroundColor: (theme) => theme.palette.black.light,
+            },
+          },
+        }}
       >
         {sections?.map((s, index) => (
-          <MenuItem key={index} onClick={handleCloseMenu}>
+          <MenuItem key={index} onClick={handleCloseMenu} className="centered">
             <Link
               href={`#${s.name}`}
+              width={1}
               sx={{
                 color: active === s.name ? "secondary.main" : "white.main",
                 transition: "color 0.3s",
               }}
             >
-              <Typography variant="h6" noWrap>
+              <Typography variant="h6" noWrap textAlign="center" width={1}>
                 {s.text}
               </Typography>
             </Link>
           </MenuItem>
         ))}
+
+        <Box width={1} className="centered" my={2}>
+          <Button variant="outlined">resume</Button>
+        </Box>
       </Menu>
     </>
   );

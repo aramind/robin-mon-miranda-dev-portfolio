def3ec043c3ffefc476a510a1254af49fdd26044
aramind <88814622+aramind@users.noreply.github.com>
2025-05-26 00:39:53 +0800

    feat: making the landing page responsive

--- src/sections/landing/Landing.jsx
+++ src/sections/landing/Landing.jsx
@@ -1,39 +1,50 @@
 import { Box, Button, Stack, Typography } from "@mui/material";
+import useIsInMobile from "../../hooks/useIsInMobile";
 
 const Landing = () => {
+  const isInMobile = useIsInMobile();
   return (
     <Box
+      px={2}
       height="80vh"
-      pl={10}
+      pl={{ md: 10 }}
       alignContent="center"
       direction="row"
-      width={{ md: "70vw" }}
+      width={{ xs: "100%", md: "70vw" }}
     >
       <Stack>
         <Typography fontSize="1.1rem">Hi, my name is</Typography>
-        <Typography variant="h2" fontWeight="bold" color="primary">
+        <Typography
+          variant={isInMobile ? "h3" : "h2"}
+          fontWeight="bold"
+          color="primary"
+        >
           Robin Mon Miranda
         </Typography>
-        <Typography variant="h4" fontWeight="bold" color="primary.light">
+        <Typography
+          variant={isInMobile ? "h5" : "h4"}
+          fontWeight="bold"
+          color="primary.light"
+        >
           Engineer. Educator. Now, to build with code.
         </Typography>
         <br />
-        <Box width="50%">
-          <Typography fontSize="1.2rem">
+        <Box width={{ xs: 1, md: "50%" }}>
+          <Typography fontSize={{ xs: "1rem", md: "1.2rem" }}>
             I’m an electronics engineer transitioning into software development.
             I’m currently open to work and excited to begin a career where I can
             provide solutions through code.
           </Typography>
           <Stack
-            direction="row"
+            direction={{ xs: "column", md: "row" }}
             justifyContent="space-between"
             spacing={2}
             mt={2}
           >
-            <Button variant="outlined" fullWidth>
+            <Button variant="outlined" fullWidth sx={{ py: 1.5 }}>
               CHECK MY RESUME
             </Button>
-            <Button variant="contained" fullWidth>
+            <Button variant="contained" fullWidth sx={{ py: 1.5 }}>
               SHOOT ME A MESSAGE
             </Button>
           </Stack>

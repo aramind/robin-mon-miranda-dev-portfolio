42c808010bc5522beb304da164c7fc88c6691cb4
aramind <88814622+aramind@users.noreply.github.com>
2025-05-25 21:49:02 +0800

    feat: defining the colors in the theme

---
+++ src/themes/dark.js
@@ -0,0 +1,40 @@
+const { createTheme } = require("@mui/material");
+
+const COLORS = {
+  primary: {
+    dark: "#058572",
+    main: "#22D1AE",
+    light: "#AAEEE0",
+  },
+  secondary: {
+    dark: "#CCA114",
+    main: "#E9BB1D",
+    light: "#F2D77D",
+  },
+  black: {
+    dark: "#0D0D15",
+    main: "#1E1C29",
+    light: "#3D4655",
+  },
+  white: {
+    dark: "#D6D6D6",
+    main: "#E0E0E0",
+    light: "#F5F5F5",
+  },
+};
+
+const darkTheme = createTheme({
+  palette: {
+    primary: COLORS.primary,
+    secondary: COLORS.secondary,
+    black: COLORS.black,
+    white: COLORS.white,
+  },
+  typography: {
+    fontFamily: `"Inter", "Nunito Sans", "Roboto", sans-serif`,
+    condensed: `"Barlow Condensed", sans-serif`,
+    poppins: `"Poppins", sans-serif`,
+  },
+});
+
+export default darkTheme;

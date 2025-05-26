61cf7f561b993fe7ca7631258ac245962bf82fb3
aramind <88814622+aramind@users.noreply.github.com>
2025-05-26 13:29:56 +0800

    refactor: renaming to job highlights

--- src/sections/experiences/JobHighlights.jsx
+++ src/sections/experiences/JobHighlights.jsx
@@ -1,11 +1,11 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
 
-const JobDescription = ({ descriptions = [] }) => {
-  if (!Array.isArray(descriptions)) return;
+const JobHighlights = ({ highlights }) => {
+  if (!Array.isArray(highlights)) return;
   return (
     <Box component="ul" pl={2} sx={{ listStyleType: "disc" }}>
-      {descriptions?.map((d, index) => (
+      {highlights?.map((d, index) => (
         <Typography key={index} component="li" variant="body2">
           {d}
         </Typography>
@@ -14,4 +14,4 @@ const JobDescription = ({ descriptions = [] }) => {
   );
 };
 
-export default JobDescription;
+export default JobHighlights;

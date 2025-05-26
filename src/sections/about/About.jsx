2dc614f172b32e0de76f14310f6e4105034d03b0
aramind <88814622+aramind@users.noreply.github.com>
2025-05-26 17:18:56 +0800

    feat: adding about me section

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { Fragment } from "react";
 import SectionTitle from "../../components/SectionTitle";
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+import useIsInMobile from "../../hooks/useIsInMobile";
 
+const Highlighted = ({ children }) => (
+  <Box
+    component="span"
+    color="secondary.lightest"
+    fontWeight="bold"
+    sx={{
+      display: "inline",
+      borderBottom: "1.5px solid",
+      borderColor: (theme) => theme.palette.secondary.main,
+    }}
+  >
+    {children}
+  </Box>
+);
+
+const ParagraphWrapper = ({ children }) => (
+  <Typography variant="body2" textAlign="justify" fontSize={{ md: "1.2rem" }}>
+    {children}
+  </Typography>
+);
 const About = () => {
+  const isInMobile = useIsInMobile();
   return (
     <Box
       width={1}
@@ -11,11 +33,89 @@ const About = () => {
       id="about"
       component="section"
       px={{ xs: 2, md: 10 }}
-      className="outlined"
     >
       <SectionTitle title="About me" />
       <br />
-      <Box spacing={2} width={1} className="outlined2"></Box>
+      <Stack
+        spacing={2}
+        px={2}
+        width={{ xs: 1, md: "640px" }}
+        justifyContent="center"
+        mx="auto"
+      >
+        <Typography
+          textAlign="center"
+          fontSize={{ xs: "2rem", md: "2.5rem" }}
+          fontWeight="bold"
+          color="primary"
+        >
+          ROBIN MON MIRANDA
+        </Typography>
+        <Stack
+          direction="row"
+          spacing={1}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Box
+            height="192px"
+            width="192px"
+            sx={{ backgroundColor: "primary.main" }}
+          />
+          {!isInMobile && (
+            <>
+              <Box
+                height="192px"
+                width="192px"
+                sx={{ backgroundColor: "primary.main" }}
+              />
+              <Box
+                height="192px"
+                width="192px"
+                sx={{ backgroundColor: "primary.main" }}
+              />
+            </>
+          )}
+        </Stack>
+        <ParagraphWrapper>
+          I come from a background in{" "}
+          <Highlighted>electronics engineering</Highlighted>, with years of
+          experience as a college and review instructor—helping future engineers
+          pass and excel in their licensure exams.
+        </ParagraphWrapper>
+        <ParagraphWrapper>
+          While I love teaching and mentoring, what truly energized me were the
+          moments I had to think of solutions to specific problems: designing
+          specialized materials, creating systems tailored to unique needs, and
+          figuring out how to make ideas work in practice. That’s where I
+          discovered my <Highlighted>passion — building solutions</Highlighted>.
+        </ParagraphWrapper>
+        <ParagraphWrapper>
+          This led me to <Highlighted>explore software development</Highlighted>
+          , a field where I can continue doing what I love: thinking critically
+          and turning ideas into tools that solve problems.
+        </ParagraphWrapper>
+        <ParagraphWrapper>
+          Now, I’m actively <Highlighted>transitioning</Highlighted> into this
+          new path. I’ve recently{" "}
+          <Highlighted>
+            completed a full-stack web development bootcamp
+          </Highlighted>
+          , where I trained in the <Highlighted>MERN stack</Highlighted>{" "}
+          (MongoDB, Express, React, Node.js) and added Material UI through
+          creating side projects.
+        </ParagraphWrapper>
+        <ParagraphWrapper>
+          Tough I’m just starting out, I bring with me a strong{" "}
+          <Highlighted>foundation in logic and systems thinking</Highlighted>, a
+          <Highlighted>problem-solver’s mindset</Highlighted>, a{" "}
+          <Highlighted>passion for building</Highlighted>, and a deep{" "}
+          <Highlighted>
+            commitment to continuous learning and growth
+          </Highlighted>
+          .
+        </ParagraphWrapper>
+      </Stack>
       <br />
     </Box>
   );
